ebae7e292ca136ad82fca551125a1a33f6aeb583
Time-xg <554621730@qq.com>
2019-05-20 15:40:36 +0800

    Update Modular

--- app/src/main/java/top/timewl/localculture/Activity/MainActivity.java
+++ app/src/main/java/top/timewl/localculture/Activity/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import top.timewl.localculture.Fragment.FindFragment;
 import top.timewl.localculture.Fragment.GuideFragment;
 import top.timewl.localculture.Fragment.HomeFragment;
 import top.timewl.localculture.Fragment.MyFragment;
@@ -22,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView guide_tv;
     private Button my;
     private TextView my_tv;
+    private Button find;
+    private LinearLayout find_layout;
+    private TextView find_tv;
+    private LinearLayout home_layout;
+    private LinearLayout guide_layout;
+    private LinearLayout my_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void listener() {
-        home.setOnClickListener(new View.OnClickListener() {
+        home_layout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 removeActivation();
                 home.setEnabled(false);
                 home_tv.setTextColor(Color.parseColor("#1296db"));
@@ -42,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction transaction = manager.beginTransaction();
                 HomeFragment fileFragment = new HomeFragment();
                 transaction.replace(R.id.fragment_container,fileFragment,"file");
+                transaction.commit();
+            }
+        });
+        find_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeActivation();
+                find.setEnabled(false);
+                find_tv.setTextColor(Color.parseColor("#1296db"));
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                FindFragment findFragment = new FindFragment();
+                transaction.replace(R.id.fragment_container,findFragment,"find");
                 transaction.commit();
             }
         });
@@ -58,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
-        my.setOnClickListener(new View.OnClickListener() {
+        my_layout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 removeActivation();
                 my.setEnabled(false);
                 my_tv.setTextColor(Color.parseColor("#1296db"));
@@ -71,23 +92,32 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+
     }
 
     private void removeActivation() {
         home.setEnabled(true);
         guide.setEnabled(true);
         my.setEnabled(true);
+        find.setEnabled(true);
         home_tv.setTextColor(Color.parseColor("#8a8a8a"));
         guide_tv.setTextColor(Color.parseColor("#8a8a8a"));
         my_tv.setTextColor(Color.parseColor("#8a8a8a"));
+        find_tv.setTextColor(Color.parseColor("#8a8a8a"));
 
     }
 
     private void initView() {
+        home_layout = findViewById(R.id.home_layout);
         home = findViewById(R.id.home);
         home_tv = findViewById(R.id.home_tv);
+        find_layout = findViewById(R.id.find_layout);
+        find = findViewById(R.id.find);
+        find_tv = findViewById(R.id.find_tv);
+        guide_layout = findViewById(R.id.guide_layout);
         guide = findViewById(R.id.guide);
         guide_tv = findViewById(R.id.guide_tv);
+        my_layout = findViewById(R.id.my_layout);
         my = findViewById(R.id.my);
         my_tv = findViewById(R.id.my_tv);
     }
